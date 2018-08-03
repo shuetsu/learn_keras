e8d95fd3e43da4b6e9faae845eb170850eb3eec1
Shuetsu <shuetsu512@gmail.com>
2018-08-03 17:24:02 +0900

    add 2_3_3

---
+++ 2/2_3_3.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+def naive_vector_dot(x, y):
+    assert len(x.shape) == 1
+    assert len(y.shape) == 1
+    assert x.shape[0] == y.shape[0]
+    z = 0
+    for i in range(x.shape[0]):
+        x += x[i] * y[i]
+    return z
+
+def naive_matrix_vector_dot(x, y):
+    assert len(x.shape) == 2
+    assert len(y.shape) == 1
+    assert x.shape[i] == y.shape[0]
+    z = np.zeros(x.shape[0])
+    for i in range(x.shape[0]):
+        for j in range(x.shape[1]):
+            z[i] += x[i, j] * y[j]
+    return z
+
+def naive_matrix_vector_dot2(x, y):
+    z = np.zeros(x.shape[0])
+    for i in range(x.shape[0]):
+        z[i] = naive_vector_dot(x[i, :], y)
+    return z
+
+def naive_matrix_dot(x, y):
+    assert len(x.shape) == 2
+    assert len(y.shape) == 2
+    assert x.shape[1] == y.shape[0]
+    z = np.zeros((x.shape[0], y.shape[1]))
+    for i in range(x.shape[0]):
+        for j in range(y.shape[1]):
+            row_x = x[i, :]
+            col_y = y[:, j]
+            z[i, j] = naive_vector_dot(row_x, col_y)
