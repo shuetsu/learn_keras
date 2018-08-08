3b57b96d5ec68b25fe73f1f1eb0139e71eb016f8
Shuetsu <shuetsu512@gmail.com>
2018-08-08 13:30:12 +0900

    add 3_4_2.py

--- 3/3_4_1.py
+++ 3/3_4_1.py
@@ -1,11 +1,16 @@
+import numpy as np
+from keras import models
+from keras import layers
 from keras.datasets import imdb
 
-(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=1000)
 
-print(train_data[0])
+print(train_data.shape)
+print(train_labels.shape)
 
 word_index = imdb.get_word_index()
 reverse_word_index = {value:key for (key, value) in word_index.items()}
 decoded_review = ' '.join(reverse_word_index.get(i - 3, '?') for i in train_data[0])
 
+print(train_data[0])
 print(decoded_review)
