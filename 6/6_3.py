0c2dfcee83e892d21443368dcda9feaaf4c7db21
Shuetsu <shuetsu512@gmail.com>
2018-08-28 14:42:31 +0900

    add 6_2-4.py

---
+++ 6/6_3.py
@@ -0,0 +1,17 @@
+from keras.preprocessing.text import Tokenizer
+
+samples = ['The cat sat on the mat.', 'The dog ate my homework.']
+
+tokenizer = Tokenizer(num_words=100)
+
+tokenizer.fit_on_texts(samples)
+
+sequences = tokenizer.texts_to_sequences(samples)
+print(sequences)
+
+one_hot_results = tokenizer.texts_to_matrix(samples, mode='binary')
+print(one_hot_results.shape)
+print(one_hot_results)
+
+word_index = tokenizer.word_index
+print('Found %s unique tokens.' % len(word_index))
