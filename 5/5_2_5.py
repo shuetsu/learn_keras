a92ded22e5c42959f7a03a45a6102dcfaa29cbd9
Shuetsu <shuetsu512@gmail.com>
2018-09-07 17:10:34 +0900

    add 5_2_3 - 5_3_1

---
+++ 5/5_2_5.py
@@ -0,0 +1,37 @@
+import os, shutil
+import matplotlib.pyplot as plt
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
+
+base_dir = '../../learn_keras_data/cats_and_dogs_small'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+test_dir = os.path.join(base_dir, 'test')
+train_cats_dir = os.path.join(train_dir, 'cats')
+
+datagen = ImageDataGenerator(rescale=0,
+                             rotation_range=40,
+                             width_shift_range=0.2,
+                             height_shift_range=0.2,
+                             shear_range=0.2,
+                             zoom_range=0.2,
+                             horizontal_flip=True,
+                             fill_mode='nearest')
+
+fnames = [os.path.join(train_cats_dir, fname) for fname in os.listdir(train_cats_dir)]
+
+img_path = fnames[0]
+
+img = image.load_img(img_path, target_size=(150, 150))
+x = image.img_to_array(img)
+x = x.reshape((1,) + x.shape)
+
+i = 0
+for batch in datagen.flow(x, batch_size=1):
+    plt.figure(i)
+    imgplot = plt.imshow(image.array_to_img(batch[0]))
+    i += 1
+    if i % 4 == 0:
+        break
+
+plt.show()
--- 5/5_2_6.py
+++ 5/5_2_6.py
@@ -10,19 +10,26 @@ train_dir = os.path.join(base_dir, 'train')
 validation_dir = os.path.join(base_dir, 'validation')
 test_dir = os.path.join(base_dir, 'test')
 
-train_datagen = ImageDataGenerator(rescale=1./255)
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                   rotation_range=40,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True)
+
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 train_generator = train_datagen.flow_from_directory(
   train_dir,
   target_size=(150,150),
-  batch_size=20,
+  batch_size=32,
   class_mode='binary')
 
 validation_generator = train_datagen.flow_from_directory(
   validation_dir,
   target_size=(150,150),
-  batch_size=20,
+  batch_size=32,
   class_mode='binary')
 
 for data_batch, labels_batch in train_generator:
@@ -40,6 +47,7 @@ model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(128, (3,3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
 model.add(layers.Dense(512, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 
@@ -47,8 +55,8 @@ model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=1e-4),
 
 print(model.summary())
 
-history = model.fit_generator(train_generator, steps_per_epoch=100, epochs=10, validation_data=validation_generator, validation_steps=50)
-model.save('cats_and_dogs_small_1.h5')
+history = model.fit_generator(train_generator, steps_per_epoch=100, epochs=30, validation_data=validation_generator, validation_steps=50)
+model.save('cats_and_dogs_small_2.h5')
 
 acc = history.history['acc']
 val_acc = history.history['val_acc']
